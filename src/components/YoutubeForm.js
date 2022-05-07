460e18afff720dcc917d30140143be0b370a997a
sumanta2 <sumantagorai.21@gmail.com>
2022-05-07 13:52:09 +0530

    6 use onBlur Method

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -41,26 +41,29 @@ const YoutubeForm = () => {
   })
   //console.table('Form Values', formik.values)
   //console.log('Form error',formik.errors)
+  console.log('Visited field',formik.touched)
   return (
     <div>
       <form onSubmit={formik.handleSubmit} >
       <div className="form-control">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" onChange={formik.handleChange} value={formik.values.name} id="name" />
+        <input type="text" name="name" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} id="name" />
         {formik.errors.name ? <div className="error">{formik.errors.name}</div>: null }
         </div>
 
         <div className="form-control">
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" onChange={formik.handleChange} value={formik.values.email} id="email" />
+        <input type="email" name="email" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} id="email" />
         {formik.errors.email ? <div className="error">{formik.errors.email}</div>: null }
         </div>
 
         <div className="form-control">
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" onChange={formik.handleChange} value={formik.values.channel} id="channel" />
+        <input type="text" name="channel" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.channel} id="channel" />
         {formik.errors.channel ? <div className="error">{formik.errors.channel}</div>: null }
         </div>
+
+        {/* onBlur event help to track any component are already visited or not */}
 
         <button type="submit">Submit</button>
       </form>

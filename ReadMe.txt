ce6fa2f3ffae0986e86f3d0f7e7a64660513b223
sumanta2 <sumantagorai.21@gmail.com>
2022-05-07 13:18:54 +0530

    4 add form validation

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,5 @@
 for install formik package  ::  npm install formik
 
-vdo:: 5
+vdo:: 7
 
 playlist link::  https://youtube.com/playlist?list=PLC3y8-rFHvwiPmFbtzEWjESkqBVDbdgGu
--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,15 +1,41 @@
 import { useFormik } from 'formik';
 const YoutubeForm = () => {
-  const formik = useFormik({
-    initialValues:
-    {
-      name: '',
-      email: '',
-      channel: '',
-    },      //the key name must be same of the name of input tag's name property's value
-    onSubmit:(values)=>{
-      console.table('Form Values',values)
+
+
+  const onSubmit=(values) => {
+    console.table('Form Values', values)
+  }
+
+  const validate =(values) => {
+
+    let errors = {}
+    if (!values.name) {
+      errors.name = "Required"
     }
+    if (!values.email) {
+      errors.email = "Required"
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))
+    {
+      errors.email = 'Invalid email format'
+    }
+    if (!values.channel) {
+      errors.channel = "Required"
+    }
+
+  }
+  
+  const initialValues=
+  {
+    name: '',           //the key name must be same of the name of input tag's name property's value
+    email: '',
+    channel: '',
+  }
+
+  const formik = useFormik({
+    initialValues,    
+    onSubmit,
+    validate
   })
   //console.table('Form Values', formik.values)
   return (

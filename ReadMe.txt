f0f51bbbdf128ca378fbdf83e1f72545d42f2374
sumanta2 <sumantagorai.21@gmail.com>
2022-05-08 09:44:33 +0530

    10 Formik,Form,Field,ErrorMessage component

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,6 +2,8 @@ for install formik package  ::  npm install formik
 
 for install yup package   :: npm install yup   it used to user input validation
 
-vdo:: 12
+vdo:: 13,14,15,16
 
 playlist link::  https://youtube.com/playlist?list=PLC3y8-rFHvwiPmFbtzEWjESkqBVDbdgGu
+
+this commit's previous commit (commit no 12 according to github) which format we used this follow until learn all formik  property properly 
--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,69 +1,72 @@
-import { useFormik } from 'formik';
+import { useFormik, Formik, Form,Field,ErrorMessage } from 'formik';
 import * as Yup from 'yup'
 
 const YoutubeForm = () => {
 
 
-  const onSubmit=(values) => {
+  const onSubmit = (values) => {
     console.table('Form Values', values)
   }
 
-  const validationSchema=Yup.object({
-    name:Yup.string().required('Required'),
-    email:Yup.string().email('Invalid email format').required('Required'),
-    channel:Yup.string().required("Required")
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email format').required('Required'),
+    channel: Yup.string().required("Required")
   })
-  
-  const initialValues=
+
+  const initialValues =
   {
     name: '',           //the key name must be same of the name of input tag's name property's value
     email: '',
     channel: '',
   }
 
-  const formik = useFormik({
-    initialValues,    
-    onSubmit,
-    validationSchema
-  })
+  // const formik = useFormik({
+  //   initialValues,
+  //   onSubmit,
+  //   validationSchema
+  // })
   //console.table('Form Values', formik.values)
   //console.log('Form error',formik.errors)
   //console.log('Visited field',formik.touched)
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit} >
-      <div className="form-control">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name"
-        //onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} 
-        {...formik.getFieldProps('name')}  //here name parameter value is the name of this input element,It automatically manage onBlur,onChange,value property
-         />
-        {formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div>: null }
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <Form >
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <Field type="text" name="name" id="name"   //Field tag of Formik work same as getFieldProps method
+            //onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} 
+            // {...formik.getFieldProps('name')}  //here name parameter value is the name of this input element,It automatically manage onBlur,onChange,value property
+          />
+          {/* {formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div> : null} */}
+          <ErrorMessage name='name'/>   {/* it work same as previous line but message display as a text */}
         </div>
 
         <div className="form-control">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email"
-        // onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email}  
-          {...formik.getFieldProps("email")}  //here email parameter value is the name of this input element,It automatically manage onBlur,onChange,value property
-        />
-        {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div>: null }
+          <label htmlFor="email">Email</label>
+          <Field type="email" name="email" id="email"       //Field tag of Formik work same as getFieldProps method
+            // onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email}  
+            // {...formik.getFieldProps("email")}  //here email parameter value is the name of this input element,It automatically manage onBlur,onChange,value property
+          />
+          {/* {formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null} */}
+          <ErrorMessage name='email'/>    {/* it work same as previous line but message display as a text */}
         </div>
 
         <div className="form-control">
-        <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" 
-        // onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.channel} 
-        {...formik.getFieldProps("channel")}  //here channel parameter value is the name of this input element,It automatically manage onBlur,onChange,value property
-        />
-        { formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div>: null }
+          <label htmlFor="channel">Channel</label>
+          <Field as='textarea' type="text" name="channel" id="channel"      //Field tag of Formik work same as getFieldProps method
+            // onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.channel} 
+            // {...formik.getFieldProps("channel")}  //here channel parameter value is the name of this input element,It automatically manage onBlur,onChange,value property
+          />
+          {/* {formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div> : null} */}
+           <ErrorMessage name='channel'/>   {/* it work same as previous line but message display as a text */}
         </div>
 
         {/* onBlur event help to track any component are already visited or not */}
 
         <button type="submit">Submit</button>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   )
 }
 
